7dc9a0ba82a03ea0734603c97dc0b5af33008f0b
Harinarayan Sreenivasan <hsreenivasan@ebay.com>
2014-12-18 13:37:01 +0530

    bootstrap and jquery

--- rip/views.py
+++ rip/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+# Service views
+def list_services(request):
+	return HttpResponse("")
+
+def add_service(request):
+	return HttpResponse("")
+
+def edit_service(request):
+	return HttpResponse("")
+
+def delete_service(request):
+	return HttpResponse("")
+
+def test_service(request):
+	return HttpResponse("")
+
+
+# Operation views
+def list_operations(request):
+	return HttpResponse("")
+
+def add_operation(request):
+	return HttpResponse("")
+
+def edit_operation(request):
+	return HttpResponse("")
+
+def delete_operation(request):
+	return HttpResponse("")
+
+def test_operation(request):
+	return HttpResponse("")
+
+
+# TestCase views
+def list_testcases(request):
+	return HttpResponse("")
+
+def add_testcase(request):
+	return HttpResponse("")
+
+def edit_testcase(request):
+	return HttpResponse("")
+
+def delete_testcase(request):
+	return HttpResponse("")
+
+def test_testcase(request):
+	return HttpResponse("")
+
