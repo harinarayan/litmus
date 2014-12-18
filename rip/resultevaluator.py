e51bd0c93aca2a25f82a1eb67c1af1437c0ca033
rajesh <rkumar6@paypal.com>
2014-12-19 03:02:27 +0530

    service caller and condition evaluator

---
+++ rip/resultevaluator.py
@@ -0,0 +1,73 @@
+import json, traceback 
+from jsonpath_rw import jsonpath, parse
+
+class ResultEvaluator:
+	def get_json_value(self, json_object, json_expression):
+		value_list = []
+		ds = parse(json_expression).find(json_object)
+		for d in ds:
+			value_list.append(d.value)
+			value_list.append(type(d.value))
+		return value_list
+	
+	def match_json_value(self, json_object, json_expression, operator, expected_value):
+		return_dict = {}
+		actual_json_value = self.get_json_value(json.loads(json_object), json_expression)
+
+		if len(actual_json_value) < 2:
+			return_dict["status"] = "Failed"
+			return_dict["reason"] = "Failed to retrieve value from response json based on given expression"
+			return return_dict
+
+		converted_expected_value_list = []
+		converted_expected_value = ""
+		try:
+			if type(expected_value) == list:
+				for data in expected_value:
+					converted_expected_value_list.append(actual_json_value[1](data))
+			else:
+				converted_expected_value = actual_json_value[1](expected_value)
+		except ValueError:
+			return_dict["status"] = "Failed"
+			return_dict["reason"] = traceback.format_exc()
+			return return_dict
+
+		if operator == "EQ":
+			if converted_expected_value == actual_json_value[0]:
+				return_dict["status"] = "Passed"
+				return return_dict
+			else:
+				return_dict["status"] = "Failed"
+				return_dict["reason"] = "Expected value => " + converted_expected_value + " Actual value => " + actual_json_value[0]
+				return return_dict
+		elif operator == "NE":
+			if converted_expected_value != actual_json_value[0]:
+				return_dict["status"] = "Passed"
+				return return_dict
+			else:
+				return_dict["status"] = "Failed"
+				return_dict["reason"] = "Expected value => " + converted_expected_value + " Actual value => " + actual_json_value[0]
+				return return_dict
+
+		elif operator == "IN":
+			if converted_expected_value_list.count(actual_json_value[0]) > 0:
+				return_dict["status"] = "Passed"
+				return return_dict
+			else:
+				return_dict["status"] = "Failed"
+				return_dict["reason"] = "Expected value list => " + ', '.join(str(value) for value in converted_expected_value_list) + " Actual value => " + actual_json_value[0]
+				return return_dict
+
+		elif operator == "NI":
+			if converted_expected_value_list.count(actual_json_value[0]) == 0:
+				return_dict["status"] = "Passed"
+				return return_dict
+			else:
+				return_dict["status"] = "Failed"
+				return_dict["reason"] = "Expected value list => " + ', '.join(str(value) for value in converted_expected_value_list) + " Actual value => " + actual_json_value[0]
+				return return_dict
+		else:
+			return_dict["status"] = "Failed"
+			return_dict["reason"] = traceback.format_exc()
+			return return_dict
+
---
+++ rip/testengine.py
@@ -0,0 +1,127 @@
+from mock_models import Service, Operation, TestCase, Condition
+import json
+import httplib
+from resultevaluator import ResultEvaluator
+
+class Evaluate:
+	
+	def __init__(self):
+		self.service=""
+		self.operation=""
+		self.testcase=""
+
+	def evaluate_testcase(self, service, operation, testcase):
+		eval_result_list={}
+		host = service.host
+		port = service.port
+		operation_method = operation.method
+		operation_url = self.get_complete_url(operation, testcase)
+		
+		server_output = self.get_server_output(host, port, operation_url, operation_method)
+
+		if server_output["status"] != testcase.exp_http_response:
+			eval_result_list["status"] = "Failed"
+			eval_result_list["reason"] = "HTTP response mismatch. Status => " + str(server_output["status"]) + " Reason => " + server_output["reason"]
+			return eval_result_list
+
+		testcase_id = testcase.id
+		condition_list = self.get_eval_conditions(testcase_id)
+
+		if len(condition_list) > 0:
+			try:
+				json_data = json.loads(server_output["json_data"])
+			except ValueError:
+				eval_result_list["status"] = "Failed"
+				eval_result_list["reason"] = "There are conditions defined on JSON data received from server but Output from server is not a valid JSON data"
+				return eval_result_list
+
+		conditions_eval_result = {}
+		for condition in condition_list:
+			result_evaluator = ResultEvaluator()
+			eval_result = result_evaluator.match_json_value(server_output["json_data"], condition.field, condition.operator, condition.value)
+			conditions_eval_result[condition.id] = eval_result
+
+		status = "Passed"
+		for condition_id, result in conditions_eval_result.items():
+			if "Passed" != result["status"]:
+				status = "Failed"
+				break
+		
+		if "Passed" == status:
+			eval_result_list["status"] = "Passed"
+		else:
+			eval_result_list["status"] = "Failed"
+			eval_result_list["reason"] = "One or More Condition evaluation Failed"
+
+		eval_result_list["conditions"] = conditions_eval_result
+		return eval_result_list
+	
+	def get_server_output(self, host, port, url, method):
+		server_response = {}
+		conn = httplib.HTTPConnection(host, port)
+		conn.request(method, url)
+		res = conn.getresponse()
+		server_response["status"] = res.status
+		server_response["reason"] = res.reason
+		server_response["json_data"] = res.read()
+		
+		return server_response
+		
+	
+	def get_complete_url(self, operation, testcase):
+		opetation_url = operation.url
+
+		tc_url_kwargs = testcase.url_kwargs
+		tc_url_params = testcase.url_params
+		tc_url_kwargs_json = json.loads(tc_url_kwargs)
+		tc_url_params_json = json.loads(tc_url_params)
+		tc_url_kwargs_dict = json.dumps(tc_url_kwargs_json)
+		tc_url_params_dict = json.dumps(tc_url_params_json)
+
+		if len(tc_url_kwargs_dict) > 0:
+			for tc_url_key,tc_url_val in tc_url_kwargs_json.items():
+				replace_str = "{" + tc_url_key + "}"
+				opetation_url = opetation_url.replace(replace_str, tc_url_val)
+
+		if len(tc_url_params_dict) > 0:
+			opetation_url = opetation_url + "?"
+			for tc_url_key, tc_url_val in tc_url_params_json.items():
+				opetation_url = opetation_url + tc_url_key + "=" + tc_url_val + "&"	
+			opetation_url = opetation_url[:-1]
+
+		return opetation_url
+
+
+	def get_eval_conditions(self, testcase_id):
+		condition_list = []
+		condition = Condition()
+		condition.id = 1
+		condition.field = "transaction_request.merchant"
+		condition.operator = "EQ"
+		condition.value = "67235"
+		condition_list.append(condition)
+
+		condition = Condition()
+		condition.id = 2
+		condition.field = "transaction_request.merchant"
+		condition.operator = "NE"
+		condition.value = "67335"
+		condition_list.append(condition)
+
+		condition = Condition()
+		condition.id = 3
+		condition.field = "transaction_request.merchant"
+		condition.operator = "IN"
+		rval = [67335,673546,984763]
+		condition.value = rval
+		condition_list.append(condition)
+
+		condition = Condition()
+		condition.id = 4
+		condition.field = "transaction_request.merchant"
+		condition.operator = "NI"
+		rval = [67235,748373,89498,"alkddjd"]
+		condition.value = rval
+		condition_list.append(condition)
+
+		return condition_list
